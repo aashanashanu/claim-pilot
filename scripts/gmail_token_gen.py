c39e8f27440378fe741ee8a69baa1b8dad228561
aashanashanu <aashanashanu@gmail.com>
2026-09-09 23:19:43 -0500

    added terraform destroy

---
+++ scripts/gmail_token_gen.py
@@ -0,0 +1,32 @@
+import os
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send",
+]
+
+def generate_token():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        
+    # If there are no valid credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            # Looks for the credentials.json you downloaded from Google Console
+            flow = InstalledAppFlow.from_client_secrets_file('/Users/aashanashanu/Downloads/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+            
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    print("Successfully created token.json file!")
+
+if __name__ == '__main__':
+    generate_token()
